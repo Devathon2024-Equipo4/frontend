315d6cd4675215fe125f5ef22f13db6020153001
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-11-08 15:15:12 -0500

    feat: gps service

---
+++ src/services/gpsService.js
@@ -0,0 +1,12 @@
+import api from "./api";
+import { URI_GPS } from "./endpoints";
+
+export const create = async ({ address }) => {
+  try{
+    const response = await api.post(URI_GPS, { address });
+    return response.data;
+  }catch(error){
+    throw new Error(error.response ? error.response.data : 'Unknown error');
+  } 
+
+};
