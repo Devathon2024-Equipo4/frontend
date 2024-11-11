1ea4907049882686656f56ab023742f846c2fe4b
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-11-11 18:30:49 -0500

    feat: add ipdetails in store

--- src/stores/gpsStore.js
+++ src/stores/gpsStore.js
@@ -2,12 +2,18 @@ import { create } from "zustand";
 import { createJSONStorage, devtools, persist } from "zustand/middleware";
 
 const GpsApi = (set) => ({
+  ipDetails: [],
+  latitude: 0.00,
+  longitude: 0.00, 
   addresses : [],
   coordinates: [],
   position: [51.505, -0.09],
   setAddresses: (newAddresses) => set({ addresses: newAddresses }, false, "SET_GPS_ADDRESSES"),
   setCoordinates: (newCoordinates) => set({ coordinates: newCoordinates }, false, "SET_GPS_COORDINATES"),
   setPosition: (newPosition) => set({ position: newPosition }, false, "SET_GPS_POSITION"),
+  setIpDetails: (newIpDetails) => set({ ipDetails: newIpDetails }, false, "SET_GPS_IP_DETAILS"),
+  setLatitude: (newLatitude) => set({ latitude: newLatitude }, false, "SET_GPS_LATITUDE"),
+  setLongitude: (newLongitude) => set({ longitude: newLongitude }, false, "SET_GPS_LONGITUDE"),
 });
 
 export const useGpsStore = create(
