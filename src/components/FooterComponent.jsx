2d34c8a0ca60d8a7386f6e8ef75c56637ec885d9
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-11-20 09:42:03 -0500

    feat: christmas footer

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
 
 const FooterComponent = () => {
   return (
-    <div className='flex flex-col bg-donJuan text-gray py-5 justify-center items-center h-16'>
+    <div className='footer flex flex-col bg-donJuan text-gray py-5 justify-center items-center h-16'>
         <h1 className=''> © Devathon 2024 - Equipo 4</h1>
         <p></p>
     </div>
