830190bd90f0e423e53ce7a4f969bd35b334584f
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-12-01 23:05:04 -0500

    feat: update all error services

--- src/services/gpsServices.js
+++ src/services/gpsServices.js
@@ -1,18 +1,13 @@
 import api from "@/utils/api";
 import { URI_GPS } from "./endpoints";
+import { handleError } from "@/utils/errorHandler";
 
 export const create = async ({ address }) => {
   try{
     const response = await api.post(URI_GPS, { address });
     return response.data;
   }catch(error){
-    if (error.response) {
-      throw new Error(error.response.data.message || 'Error en la respuesta del servidor');
-    } else if (error.request) {
-      throw new Error('No se recibió respuesta del servidor');
-    } else {
-      throw new Error('Error en la configuración de la solicitud: ' + error.message);
-    }
+    handleError(error);
   } 
 
 };
@@ -22,12 +17,6 @@ export const getRecent = async() => {
     const response = await api.get(URI_GPS);
     return response.data;
   }catch(error){
-    if (error.response) {
-      throw new Error(error.response.data.message || 'Error en la respuesta del servidor');
-    } else if (error.request) {
-      throw new Error('No se recibió respuesta del servidor');
-    } else {
-      throw new Error('Error en la configuración de la solicitud: ' + error.message);
-    }
+    handleError(error);
   }
 };
--- src/services/weatherServices.js
+++ src/services/weatherServices.js
@@ -1,6 +1,6 @@
 import api from "@/utils/api";
 import { URI_WEATHER } from "./endpoints";
-
+import { handleError } from "@/utils/errorHandler";
 
 
 export const getWeather = async (query) => {
@@ -8,13 +8,7 @@ export const getWeather = async (query) => {
     const response = await api.get(`${URI_WEATHER}/${query}`);
     return response.data;
   }catch(error){
-    if (error.response) {
-      throw new Error(error.response.data.message || 'Error en la respuesta del servidor');
-    } else if (error.request) {
-      throw new Error('No se recibió respuesta del servidor');
-    } else {
-      throw new Error('Error en la configuración de la solicitud: ' + error.message);
-    }
+    handleError(error);
   } 
 
 };
